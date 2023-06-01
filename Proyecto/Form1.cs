a68b74c55479017f35ec80bd785ed8cf33f749eb
PelaoSh0x <sebastianverajamett@gmail.com>
2023-06-01 01:19:26 -0400

    test3

--- Proyecto/Form1.cs
+++ Proyecto/Form1.cs
@@ -16,7 +16,7 @@ namespace Proyecto
         {
             InitializeComponent();
         }
-        //MEO POTO TEST 1
+        //MEO POTO TEST 1das
 
 
         private void ingresarToolStripMenuItem_Click(object sender, EventArgs e)
