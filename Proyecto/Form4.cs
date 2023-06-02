d0e1ba19a6956d941cf56280a498b60d1727e406
PelaoSh0x <sebastianverajamett@gmail.com>
2023-06-01 23:07:22 -0400

    TEST 4

--- Proyecto/Form4.cs
+++ Proyecto/Form4.cs
@@ -16,5 +16,10 @@ namespace Proyecto
         {
             InitializeComponent();
         }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            //JAHNFGLSKDJFHGLJSDFKJGNÑLSKDFJGKLSDFJGÑLKDASFJGÑKLSDFJGKÑLD
+        }
     }
 }
